6ff740d18f685af14d137be8faba7c41be39e223
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2023-01-27 22:50:43 -0500

    fix apostrophe error

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,7 +1,7 @@
 import React from 'react';
 export const contact = {
   info: <>
-    Let@apos;s collaborate <br />
+    Let&apos;s collaborate <br />
     Always looking for new opportunities <br/>
     Browse my gitHub repositories, or check out my linkedIn page. <br/>
     Send me an email or message me on linkedIn! </>
