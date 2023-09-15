0bcb93a33a5f072000119f34d4fb04a073e35e0d
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2023-09-15 14:59:53 -0400

    fix config file errors

--- src/config.js
+++ src/config.js
@@ -1,9 +1,15 @@
 const PORT = process.env.PORT || 3001;
 
-const EMAIL_SERVER_URI =
-  process.env.REACT_APP_ENVIRONMENT === 'development'
-    ? process.env.REACT_APP_DEV_EMAIL_SERVER_URL
-    : process.env.REACT_APP_EMAIL_SERVER_URL;
+let EMAIL_SERVER_URI;
+
+if (process.env.REACT_APP_ENVIRONMENT) {
+  EMAIL_SERVER_URI =
+    process.env.REACT_APP_ENVIRONMENT === 'development'
+      ? process.env.REACT_APP_DEV_EMAIL_SERVER_URL
+      : process.env.REACT_APP_EMAIL_SERVER_URL;
+} else {
+  EMAIL_SERVER_URI = process.env.REACT_APP_EMAIL_SERVER_URL;
+}
 
 
 
