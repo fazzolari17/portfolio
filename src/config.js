da3dda31f19dcf8c9d500b7cb6bb0f3df34eb907
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2023-09-15 15:29:40 -0400

    add console log to config file

--- src/config.js
+++ src/config.js
@@ -3,13 +3,21 @@ const PORT = process.env.PORT || 3001;
 let EMAIL_SERVER_URI;
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
+  console.log('dev ran');
   EMAIL_SERVER_URI =
     process.env.REACT_APP_ENVIRONMENT === 'development'
       ? process.env.REACT_APP_DEV_EMAIL_SERVER_URL
       : process.env.REACT_APP_EMAIL_SERVER_URL;
 } else if (process.env.NODE_ENV === 'production') {
+  console.log('prod ran');
   EMAIL_SERVER_URI = process.env.REACT_APP_EMAIL_SERVER_URL;
 }
+
+console.log('EMAIL_SERVER_URI', EMAIL_SERVER_URI);
+console.log(
+  'REACT_APP_EMAIL_SERVER_URL',
+  process.env.REACT_APP_EMAIL_SERVER_URL
+);
 
 
 
